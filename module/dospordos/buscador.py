5b77f11b5046317db6486bbe4309f7f155d17584
unoporuno <jorgegarciaflo@gmail.com>
2012-10-13 12:59:14 +0200

    description bug solved xgoogle_unoporuno

--- module/dospordos/buscador.py
+++ module/dospordos/buscador.py
@@ -52,8 +52,8 @@ import tempfile, libmorfo_python
 
 import yql
 from lxml import etree
-from xgoogle.search import GoogleSearch, SearchError
-from xgoogle.translate import LanguageDetector, DetectionError
+from xgoogle_unoporuno.search import GoogleSearch, SearchError
+from xgoogle_unoporuno.translate import LanguageDetector, DetectionError
 
 UNO_ROOT = ''
 FREELING_LIB = ''
@@ -511,8 +511,8 @@ class Buscador:
                           time.ctime(self.last_query_time))
             for r in results:
                 titulo = r.title.encode('utf8')
-                descripcion = r.desc.encode('utf8')
-                logging.debug('Buscador::DESCRIPTION_BUG::_g_busca::descripcion='+descripcion)
+                descripcion = r.desc.encode('utf8'
+                #logging.debug('Buscador::DESCRIPTION_BUG::_g_busca::descripcion='+descripcion) 12/OCT/2012
                 url = r.url.encode('utf8')
                 r_snippet = Snippet(self.consulta, titulo,
                                     descripcion, url, 'google', self.query_type)
@@ -624,7 +624,7 @@ class Snippet:
         self.query_type = query_type
         self.title = title.strip()
         self.description = description.strip()
-        logging.debug('snippet::DESCRIPTION_BUG::description='+self.description)
+        #logging.debug('snippet::DESCRIPTION_BUG::description='+self.description)
         self.description.replace('\n','§')
         if self.title != self.description:
             self.string = self.title + ' § ' + self.description
